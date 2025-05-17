9d60336a558dd1cd71a1d4dea0bdeef048282e53
iemura kazuki <kazuki.18.and.all@gmail.com>
2025-05-17 18:56:46 +0900

    課題１

--- nguess.py
+++ nguess.py
@@ -3,9 +3,11 @@ import random
 guess = int(input('guess='))
 print('Your guess is', guess)
 answer = random.randint(1, 100)
-if guess == answer :
-    print('Good guess')
-elif guess < answer:
-    print('Too low')
-else:
-    print('Too high')
+while guess != answer:
+    if guess < answer:
+        print('Too low')
+    else:
+        print('Too high')
+    guess = int(input('guess='))
+    print('Your next guess is', guess)
+print('Good guess!')
